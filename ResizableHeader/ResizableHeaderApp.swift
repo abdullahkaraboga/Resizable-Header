1f8fe729cc44e48f2bcdbe37c477e8b6cb3152ef
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-03-07 21:59:29 +0300

    Initial Commit

---
+++ ResizableHeader/ResizableHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResizableHeaderApp.swift
+//  ResizableHeader
+//
+//  Created by Abdullah Karaboğa on 7.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ResizableHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
