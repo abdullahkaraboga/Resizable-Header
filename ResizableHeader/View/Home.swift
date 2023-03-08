033d297aff6fc742e42bcf7da017cf89d212f663
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-03-08 23:44:55 +0300

    scroll animation added.

--- ResizableHeader/View/Home.swift
+++ ResizableHeader/View/Home.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct Home: View {
+    @State var offsetY: CGFloat = 0
     var body: some View {
 
         GeometryReader { proxy in
@@ -16,23 +17,31 @@ struct Home: View {
 
                 VStack {
                     HeaderView(safeAreaTop)
-                    
-                    VStack{
-                        ForEach(1...10,id:\.self) { _ in
-                            RoundedRectangle(cornerRadius: 10,style: .continuous)
+                        .offset(y: -offsetY)
+                        .zIndex(1)
+
+                    VStack {
+                        ForEach(1...10, id: \.self) { _ in
+                            RoundedRectangle(cornerRadius: 10, style: .continuous)
                                 .fill(.blue.gradient)
                                 .frame(height: 220)
                         }
-                    }
-                    .padding(15)
+                    }.padding(15)
+                        .zIndex(0)
+                }
+                    .offset(coordinateSpace: .named("SCROLL")) { offset in
+                    offsetY = offset
                 }
             }
+                .coordinateSpace(name: "SCROLL")
                 .edgesIgnoringSafeArea(.top)
         }
     }
 
     @ViewBuilder
     func HeaderView(_ safeAreaTop: CGFloat) -> some View {
+
+        let progress = -(offsetY / 80) > 1 ? -1 : (offsetY > 0 ? 0 : (offsetY / 80))
 
         VStack(spacing: 15) {
             HStack(spacing: 15) {
@@ -50,10 +59,11 @@ struct Home: View {
                         .fill(.black)
                         .opacity(0.15)
                 }
+                    .opacity(1 + progress)
             }
 
             HStack(spacing: 0) {
-                CustomButton(symbolImage: "rectangle.potrait.and.arrow.forward", title: "Deposit") {
+                CustomButton(symbolImage: "target", title: "Deposit") {
 
                 }
                 CustomButton(symbolImage: "dollarsign", title: "Withdraw") {
@@ -66,7 +76,9 @@ struct Home: View {
 
                 }
             }
-            .padding(.top, 10)
+                .padding(.horizontal, -progress * 50)
+                .padding(.top, 10)
+                .offset(y: progress * 65)
         }
             .environment(\.colorScheme, .dark)
             .padding([.horizontal, .bottom], 15)
@@ -74,11 +86,15 @@ struct Home: View {
             .background {
             Rectangle()
                 .fill(.red.gradient)
+                .padding(.bottom, -progress * 85)
         }
     }
 
     @ViewBuilder
     func CustomButton (symbolImage: String, title: String, onClick: @escaping() -> ()) -> some View {
+
+        let progress = -(offsetY / 40) > 1 ? -1 : (offsetY > 0 ? 0 : (offsetY / 40))
+
         Button {
 
         } label: {
@@ -98,6 +114,15 @@ struct Home: View {
                     .foregroundColor(.white)
             }
                 .frame(maxWidth: .infinity)
+                .opacity(1 + progress)
+                .overlay {
+                Image(systemName: symbolImage)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .opacity(-progress)
+                    .offset(y: -10)
+            }
         }
     }
 }
@@ -105,5 +130,33 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func offset(coordinateSpace: CoordinateSpace, completion: @escaping (CGFloat) -> ()) -> some View {
+
+        self.overlay {
+            GeometryReader { proxy in
+                let minY = proxy.frame(in: coordinateSpace).minY
+
+                Color.clear
+                    .preference(key: OffsetKey.self, value: minY)
+                    .onPreferenceChange(OffsetKey.self) { value in
+                    completion(value)
+
+                }
+
+            }
+        }
+
     }
 }
